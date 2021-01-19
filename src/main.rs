29eb5cc3b18befd98f664925d9e87fd3d4adb736
fate <35225215+nxtlo@users.noreply.github.com>
2021-01-19 19:56:17 +0300

    Prep

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 extern crate rocket;
+extern crate rocket_contrib;
+extern crate serde;
+
+#[macro_use]
+extern crate serde_derive;
+
+
 use std::fs::File;
 use rocket::*;
+use rocket_contrib::json::Json;
 
 #[get("/")]
 fn index() -> File {
     File::open("./static/index.html").expect("Files not found.")
 }
 
+#[get("/drop")]
+fn drop() -> Json<String> {
+    Json(String::from("Wdup"))
+}
+
+fn rocket() -> Rocket {
+    ignite()
+    .mount("/drop", routes![drop])
+    .mount("/", routes![index])
+}
+
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket()
+    .launch();
 }
